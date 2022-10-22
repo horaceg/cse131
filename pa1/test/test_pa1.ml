47369132034f0ab3cd8d51c66f305313aa474b26
Horace Guy <horace.guy.k@gmail.com>
2022-10-22 19:22:33 +0200

    add many parsing tests

--- pa1/test/test_pa1.ml
+++ pa1/test/test_pa1.ml
@@ -19,12 +19,21 @@ let t_parse_err name program expected =
 
 let f_to_s fname = Runner.string_of_file ("input/" ^ fname)
 let forty_one = "(sub1 42)"
+let forty_one_expr = EPrim1 (Sub1, ENumber 42)
 let forty = "(sub1 (sub1 42))"
+let forty_expr = EPrim1 (Sub1, EPrim1 (Sub1, ENumber 42))
 let add1 = "(add1 (add1 (add1 3)))"
+let add1_expr = EPrim1 (Add1, EPrim1 (Add1, EPrim1 (Add1, ENumber 3)))
 let def_x = "(let ((x 5)) x)"
+let x_expr = ELet ([ ("x", ENumber 5) ], EId "x")
 let def_x2 = "(let ((x 5)) (sub1 x))"
 let x2_expr = ELet ([ ("x", ENumber 5) ], EPrim1 (Sub1, EId "x"))
 let def_x3 = "(let ((x 5)) (let ((x 67)) (sub1 x)))"
+
+let x3_expr =
+  ELet
+    ([ ("x", ENumber 5) ], ELet ([ ("x", ENumber 67) ], EPrim1 (Sub1, EId "x")))
+
 let def_x4 = "(let ((x (let ((x 5)) (sub1 x)))) (sub1 x))"
 
 let x4_expr =
@@ -38,13 +47,34 @@ let xy_expr =
   ELet ([ ("x", ENumber 5); ("y", ENumber 6) ], EPrim2 (Plus, EId "x", EId "y"))
 
 let addnums = "(+ 5 10)"
+let addnums_expr = EPrim2 (Plus, ENumber 5, ENumber 10)
 let nested_add = "(+ 5 (+ 10 20))"
+
+let nested_add_expr =
+  EPrim2 (Plus, ENumber 5, EPrim2 (Plus, ENumber 10, ENumber 20))
+
 let nested_add2 = "(+ (- 10 5) 20)"
+
+let nested_add2_expr =
+  EPrim2 (Plus, EPrim2 (Minus, ENumber 10, ENumber 5), ENumber 20)
+
 let nested_arith = "(- (* (- 54 3) 2) 102)"
+
+let nested_arith_expr =
+  EPrim2
+    ( Minus,
+      EPrim2 (Times, EPrim2 (Minus, ENumber 54, ENumber 3), ENumber 2),
+      ENumber 102 )
+
 let let_nested = "(let ((x (+ 5 (+ 10 20)))) (* x x))"
+
+let let_nested_expr =
+  ELet
+    ( [ ("x", EPrim2 (Plus, ENumber 5, EPrim2 (Plus, ENumber 10, ENumber 20))) ],
+      EPrim2 (Times, EId "x", EId "x") )
+
 let failLet = "(let ((x  1) (y 1) (x 10)) x)"
 let failID = "x"
-let forty_one_expr = EPrim1 (Sub1, ENumber 42)
 
 let testFailList =
   [
@@ -81,10 +111,19 @@ let parse_case name expected inputs =
 
 let parse_suite =
   [
+    parse_case forty_one forty_one_expr forty_one;
+    parse_case forty forty_expr forty;
+    parse_case add1 add1_expr add1;
+    parse_case def_x x_expr def_x;
     parse_case def_x2 x2_expr def_x2;
+    parse_case def_x3 x3_expr def_x3;
     parse_case def_x4 x4_expr def_x4;
     parse_case def_xy xy_expr def_xy;
-    parse_case forty_one forty_one_expr forty_one;
+    parse_case addnums addnums_expr addnums;
+    parse_case nested_add nested_add_expr nested_add;
+    parse_case nested_add2 nested_add2_expr nested_add2;
+    parse_case nested_arith nested_arith_expr nested_arith;
+    parse_case let_nested let_nested_expr let_nested;
   ]
 
 let () = Alcotest.run "Parsing" [ ("parse", parse_suite) ]
